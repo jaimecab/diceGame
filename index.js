dafbbe7b7adbd518fded6f4f84e93a7a53ed672b
Jaime Cabrera <72805181+jaimecab@users.noreply.github.com>
2021-10-28 14:40:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+    rollDice();
+}
+function rollDice(){
+let randomNumber1 = Math.floor((Math.random()*6)+1)
+let randomNumber2 = Math.floor((Math.random()*6)+1)
+
+let image1 = document.querySelector(".img1")
+let image2 = document.querySelector(".img2")
+
+image1.setAttribute("src","images/dice"+randomNumber1+".png")
+image2.setAttribute("src","images/dice"+randomNumber2+".png")
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "✌️ Player 1 wins"
+}
+  else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 wins ✌️"
+  }
+  else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "😲 It's a Draw 😲"
+  }
+}
